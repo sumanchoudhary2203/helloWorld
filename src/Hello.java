5dff982a0c66acb9ddb0f8a7a6e5f3cb78fb26c6
Suman choudhary <sc108807@gmail.com>
2025-07-14 01:21:23 +0530

    new update

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Hello{
-    public static void main (string[] args){
-        system.out.println("Hello JAVA");
+    public static void main(String[] args){
+        System.out.println("Hello JAVA");
+        System.out.println("Changes in code after pushing in Github.");
     }
 
 }
