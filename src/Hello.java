9f44fe778aa57fae5fd2a8e6f31737bde7c3eaa4
Suman choudhary <sc108807@gmail.com>
2025-07-14 01:04:09 +0530

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Hello{
+    public static void main (string[] args){
+        system.out.println("Hello JAVA");
+    }
+
+}
